301be7a38f7bb7a0f23eb0feb393d4053286c8a7
Hayden Coffey <hjcoffey@wisc.edu>
2025-05-07 01:39:57 +0000

    Print damon regions

--- src/damo_report_heatmap.py
+++ src/damo_report_heatmap.py
@@ -201,8 +201,8 @@ def fmt_heats(args, address_range_idx, __records):
     return '\n'.join(lines)
 
 def pr_heats(args, __records):
-    print(args.address_range)
     for idx in range(len(args.address_range)):
+        print(args.address_range[idx])
         print(fmt_heats(args, idx, __records))
 
 def set_missed_args(args, records):
